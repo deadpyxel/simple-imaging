0fc20820ced4f339339a35082dfae9a8877c9009
Robson Cruz <ghost0092@gmail.com>
2020-03-12 23:44:31 -0300

    fix: Use Image class instead of Matrix

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from simple_matrix.utils import read_file, save_file
 
 
 def main(args):
-    m1 = None
-    m2 = None
+    image = None
     if args.filepath:
         for filepath in args.filepath:
-            m1 = read_file(filepath)
-    m2 = Matrix(3, 3)
-    m2.values = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-
-    m = m1.sum(m2)
-    print(m)
-    save_file("output1.txt", m)
+            image = read_file(filepath)
+    negative_image = image.negative()
+    darken_image = image.darken(50)
+    lighten_image = image.lighten(75)
+    rotate_90_image = image.rotate_90()
+    save_file("negative.pgm", negative_image)
+    save_file("darken.pgm", darken_image)
+    save_file("light.pgm", lighten_image)
+    save_file("rot90.pgm", rotate_90_image)
 
 
 if __name__ == "__main__":
