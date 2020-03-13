0d65da2d8632ba7f589c6fab8f28a620386c8cb4
Robson Cruz <ghost0092@gmail.com>
2020-03-12 23:40:15 -0300

    fix(utils): Fixed file reading
    Some situations would make the values read unparsable,
    this commit fixes that

--- simple_matrix/utils.py
+++ simple_matrix/utils.py
@@ -1,17 +1,17 @@
 from .image import Image
 
-def read_file(filepath) -> Image:
+
+def read_file(filepath: str) -> Image:
     with open(filepath) as f:
         lines = f.readlines()
-        header = lines[0]
+        header = lines[0].strip()
         lines = [[int(el) for el in line.split(" ")] for line in lines[1:]]
         m, n = lines[0]
-        max_grayscale =  lines[1]
+        max_grayscale = lines[1][0]
         image = Image(header, max_grayscale, m, n)
         matrix = lines[2]
-        matrix = [matrix[i:i+n] for i in range(0, len(matrix), n)]
+        matrix = [matrix[i : i + n] for i in range(0, len(matrix), n)]
         image.values = matrix
-        print(image)
         return image
 
 
