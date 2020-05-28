6c12b445775e93863a812948f3348a8f79e29af8
Robson Cruz <ghost0092@gmail.com>
2020-05-28 20:02:11 -0300

    refactor: reestructure validation logic for `level`
    Following the SRP the pixel level validation logic was moved.
    This means the code bcomes easier to test and more decoupled.

--- simple_imaging/image.py
+++ simple_imaging/image.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from .errors import ValidationError
 from .matrix import Matrix
 
@@ -8,8 +10,17 @@ class Image(Matrix):
         self.max_grayscale = max_grayscale
         super().__init__(m=m, n=n)
 
+    @staticmethod
+    def validate_operation_level(level: int) -> Tuple[bool, str]:
+        validity = (True, "")
+        if type(level) != int:
+            validity = (False, "type")
+        elif not (0 <= level <= 255):
+            validity = (False, "range")
+        return validity
+
     def negative(self):
-        copy_image = Image(self.header, self.max_grayscale, self.m, self.n)
+        copy_image: Image = Image(self.header, self.max_grayscale, self.m, self.n)
         for i, row in enumerate(self.values):
             for j, pixel_value in enumerate(row):
                 negative_value = max(0, min(255, 255 - pixel_value))
@@ -17,10 +28,31 @@ class Image(Matrix):
         return copy_image
 
     def darken(self, level: int):
-        if not (0 <= level <= 255):
-            raise ValidationError(
-                f"Darken operation requires values between (inclusive) 0 and 255, {level} found."
-            )
+        """Darken image method
+
+        Given a level in pixel value, this will darken the image by that much.
+        The value must obey the criteria (0<=level<=255), else an Exception is raised
+
+        Arguments:
+            level {int} -- Pixel value (as in how much) for image darkening
+
+        Raises:
+            ValueError: If provided value is not an integer
+            ValidationError: If `level`does not obey `(0<=level<=255)`
+
+        Returns:
+            Image -- A new image that has been processed by the darken operation
+        """
+        level_validity = self.validate_operation_level(level)
+        if level_validity[0] is False:
+            if level_validity[1] == "type":
+                raise ValueError(
+                    f"Darken operation expects an integer, received a {type(level)}"
+                )
+            elif level_validity[1] == "range":
+                raise ValidationError(
+                    f"Darken operation requires values between (inclusive) 0 and 255, {level} found."
+                )
         copy_image = Image(self.header, self.max_grayscale, self.m, self.n)
         for i, row in enumerate(self.values):
             for j, pixel_value in enumerate(row):
@@ -29,10 +61,31 @@ class Image(Matrix):
         return copy_image
 
     def lighten(self, level: int):
-        if not (0 <= level <= 255):
-            raise ValidationError(
-                f"Lighten operation requires values between (inclusive) 0 and 255, {level} found."
-            )
+        """Lighten image method
+
+        Given a level in pixel value, this will enlighten the image by that much.
+        The value must obey the criteria (0<=level<=255), else an Exception is raised
+
+        Arguments:
+            level {int} -- Pixel value (as in how much) for image enlightening
+
+        Raises:
+            ValueError: If provided value is not an integer
+            ValidationError: If `level`does not obey `(0<=level<=255)`
+
+        Returns:
+            Image -- A new image that has been processed by the lighten operation
+        """
+        level_validity = self.validate_operation_level(level)
+        if level_validity[0] is False:
+            if level_validity[1] == "type":
+                raise ValueError(
+                    f"Darken operation expects an integer, received a {type(level)}"
+                )
+            elif level_validity[1] == "range":
+                raise ValidationError(
+                    f"Darken operation requires values between (inclusive) 0 and 255, {level} found."
+                )
         copy_image = Image(self.header, self.max_grayscale, self.m, self.n)
         for i, row in enumerate(self.values):
             for j, pixel_value in enumerate(row):
--- tests/test_image.py
+++ tests/test_image.py
@@ -9,6 +9,9 @@ def dummy_image() -> Image:
     return Image(header="P2", max_grayscale=255, m=1, n=1)
 
 
+invalid_level_types = ["a", [], 0.01, {}, object]
+
+
 @pytest.mark.parametrize("m,n", [(0, 0), (0, 1), (-1, 0), (1, 0), (-1, -1)])
 def test_raises_exception_when_creating_image_with_invalid_dimensions(m, n):
     with pytest.raises(ValidationError):
@@ -33,7 +36,7 @@ def test_raises_validationerror_when_attempting_darken_with_invalid_int(
         dummy_image.darken(level=level)
 
 
-@pytest.mark.parametrize("level", ["a", [], 0.01, {}, object])
+@pytest.mark.parametrize("level", invalid_level_types)
 def test_raises_valueerror_when_attempting_darken_with_invalid_type(
     dummy_image: Image, level: int
 ):
@@ -49,9 +52,24 @@ def test_raises_validationerror_when_attempting_lighten_with_invalid_int(
         dummy_image.lighten(level=level)
 
 
-@pytest.mark.parametrize("level", ["a", [], 0.01, {}, object])
+@pytest.mark.parametrize("level", invalid_level_types)
 def test_raises_valueerror_when_attempting_lighten_with_invalid_type(
     dummy_image: Image, level: int
 ):
     with pytest.raises(ValueError):
         dummy_image.lighten(level=level)
+
+
+@pytest.mark.parametrize("level", invalid_level_types)
+def test_level_validation_correctly_validates_type(dummy_image: Image, level):
+    assert dummy_image.validate_operation_level(level) == (False, "type")
+
+
+@pytest.mark.parametrize("level", [-1, 256])
+def test_level_validation_correctly_validates_range(dummy_image: Image, level):
+    assert dummy_image.validate_operation_level(level) == (False, "range")
+
+
+@pytest.mark.parametrize("level", [0, 1, 100, 255])
+def test_level_validation_correctly_validates_valid_level(dummy_image: Image, level):
+    assert dummy_image.validate_operation_level(level) == (True, "")
