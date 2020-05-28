837340adebb1a8a122fee8aa6325971f087f12a7
Robson Cruz <ghost0092@gmail.com>
2020-05-28 17:15:33 -0300

    test: Fix exception raising assertion

--- tests/test_image.py
+++ tests/test_image.py
@@ -7,9 +7,8 @@ from simple_imaging.image import Image
 @pytest.mark.parametrize("m,n", [(0, 0), (0, 1), (-1, 0), (1, 0), (-1, -1)])
 def test_raises_exception_when_creating_image_with_invalid_dimensions(m, n):
     with pytest.raises(ValidationError):
-        image = Image(header="P2", max_grayscale=255, m=m, n=n)
-        assert image is None
-
+        assert Image(header="P2", max_grayscale=255, m=m, n=n)
+        
 
 def test_can_create_empty_image():
     image = Image(header="P2", max_grayscale=255, m=3, n=3)
