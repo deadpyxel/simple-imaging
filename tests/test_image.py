c27ed0379492ebd370e4c30f57866bb58e899df2
Robson Cruz <ghost0092@gmail.com>
2020-05-31 20:51:38 -0300

    test: Update image generation fixture

--- tests/test_image.py
+++ tests/test_image.py
@@ -10,10 +10,10 @@ def dummy_image() -> Image:
 
 
 @pytest.fixture
-def blank_image(x: int, y: int) -> Image:
+def blank_image(x: int, y: int, grayscale_level: int = 100) -> Image:
     img = Image(header="P2", max_grayscale=255, m=x, n=y)
     # sets 100 as value for each pixel in the image
-    img.values = [[100 for _ in range(x)] for _ in range(y)]
+    img.values = [[grayscale_level for _ in range(x)] for _ in range(y)]
     return img
 
 
