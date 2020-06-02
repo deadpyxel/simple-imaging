c73937fd56f774e10940d69abe53c84fb8dca261
Robson Cruz <ghost0092@gmail.com>
2020-06-01 21:01:06 -0300

    test: Update fixture to pass pixel data for Image

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -5,7 +5,6 @@ from simple_imaging.image import Image
 
 @pytest.fixture
 def blank_image(x: int, y: int, grayscale_level: int = 100) -> Image:
-    img = Image(header="P2", max_grayscale=255, m=x, n=y)
-    # sets 100 as value for each pixel in the image
-    img.values = [[grayscale_level for _ in range(x)] for _ in range(y)]
+    pixel_values = [[grayscale_level for _ in range(x)] for _ in range(y)]
+    img = Image(header="P2", max_grayscale=255, m=x, n=y, contents=pixel_values)
     return img
