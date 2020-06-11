d87224d9d18b07decdae965c244daa34d6026fe7
Robson Cruz <ghost0092@gmail.com>
2020-06-10 22:47:55 -0300

    fix(utils): Wrong pixel_data generation
    By iterating in `n`sized chunks, instead of getting a MxN list, we were
    getting a NXM list. This fixes it by using `m`as the chunk size.
    
    Also add and update utility functions tests.

--- simple_imaging/utils.py
+++ simple_imaging/utils.py
@@ -59,7 +59,7 @@ def parse_file_contents(file_contents: List[str]) -> Dict[str, Any]:
             f"Non-matching amount of pixels found, should have {m*n} values, found {len(data)}"
         )
     else:
-        pixel_data: List[List[int]] = [data[i : i + n] for i in range(0, len(data), n)]
+        pixel_data: List[List[int]] = [data[i : i + m] for i in range(0, len(data), m)]
         return {
             "header": header,
             "dimensions": (m, n),
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,77 @@
 import pytest
 
-from simple_imaging.errors import ValidationError
+from simple_imaging.errors import (
+    InvalidConfigsError,
+    InvalidFileError,
+    InvalidHeaderError,
+    ValidationError,
+)
 from simple_imaging.image import Image, read_file, save_file
-from simple_imaging.utils import get_split_strings
+from simple_imaging.utils import get_split_strings, parse_file_contents
 
-from .fixtures import blank_image
+from .fixtures import blank_image, expected_file_data, good_file_content
 
 
 def test_raise_exception_when_file_not_found():
     with pytest.raises(FileNotFoundError):
         read_file("not_found_file")
+
+
+def test_raises_valueerror_exception_when_parsing_empty_list():
+    with pytest.raises(ValueError):
+        parse_file_contents(file_contents=[])
+
+
+@pytest.mark.parametrize("bad_contents", [["A"], [""], [1], ["P50"], [" "]])
+def test_raises_invalidheader_exception_when_parsing_bad_header(bad_contents):
+    with pytest.raises(InvalidHeaderError):
+        parse_file_contents(file_contents=bad_contents)
+
+
+@pytest.mark.parametrize("bad_contents", [["A"], [""], [1], ["P50"], [" "]])
+def test_raises_invalidfile_exception_when_parsing_bad_header(bad_contents):
+    with pytest.raises(InvalidHeaderError):
+        parse_file_contents(file_contents=bad_contents)
+
+
+@pytest.mark.parametrize(
+    "bad_contents",
+    [
+        ["P2", "A"],
+        ["P2", "1", ""],
+        ["P2", "1", "1", ""],
+        ["P2", "1", "1", "1"],
+        ["P2", " "],
+    ],
+)
+def test_raises_invalidfile_exception_when_parsing_missing_values(bad_contents):
+    with pytest.raises(InvalidFileError):
+        parse_file_contents(file_contents=bad_contents)
+
+
+@pytest.mark.parametrize(
+    "bad_contents",
+    [["P2", "-1", "1", "1"], ["P2", "1", "-1", "1"], ["P2", "1", "1", "-1"],],
+)
+def test_raises_invalidconfig_exception_when_parsing_non_positive_values(bad_contents):
+    with pytest.raises(InvalidConfigsError):
+        parse_file_contents(file_contents=bad_contents)
+
+
+@pytest.mark.parametrize(
+    "bad_contents", [["P2", "1", "1", "1"], ["P2", "1", "1", "1", "1", "1"],],
+)
+def test_raises_invalidfile_exception_when_parsing_non_matching_dimensions(
+    bad_contents,
+):
+    with pytest.raises(InvalidFileError):
+        parse_file_contents(file_contents=bad_contents)
+
+
+@pytest.mark.parametrize("x, y", [(1, 1), (3, 2), (3, 5), (3, 3)])
+def test_returns_expected_results_from_valid_contents(
+    good_file_content, expected_file_data
+):
+    assert (
+        parse_file_contents(file_contents=good_file_content) == expected_file_data
+    ), f"non-matching file parsing"
