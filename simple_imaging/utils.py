4f2326f24afe8c7dce33190da2f2d5c45f195472
Robson Cruz <ghost0092@gmail.com>
2020-06-13 19:36:41 -0300

    refactor(utils): Apply SRP to parse_file_contents
    This change was needed to allow for the implementation of additional
    file types.

--- simple_imaging/utils.py
+++ simple_imaging/utils.py
@@ -1,5 +1,5 @@
 from itertools import chain
-from typing import Any, Dict, List, TextIO
+from typing import Any, Dict, List, TextIO, Tuple, Union
 
 from .errors import InvalidConfigsError, InvalidFileError, InvalidHeaderError
 
@@ -20,6 +20,39 @@ def get_split_strings(file_contents: TextIO) -> List[str]:
     return list(chain.from_iterable(x.strip().split() for x in file_contents))
 
 
+def _extract_header(file_contents: List[str]) -> Tuple[str, List[str]]:
+    header, *contents = file_contents  # type: str, List[str]
+    acceptable_headers = ("P1", "P2", "P3")
+    if header not in acceptable_headers:
+        raise InvalidHeaderError(f"Header {header} is not allowed or invalid")
+    return header, contents
+
+
+def _extract_dimensions(file_contents: List[str]) -> Tuple[int, int, List[int]]:
+    try:
+        # All remaining values should be integers
+        m, n, *contents = [int(x) for x in file_contents]  # type: int, int, List[int]
+    except ValueError:
+        raise InvalidFileError(f"Found invalid values (non-numerical) in file contents")
+    return m, n, contents
+
+
+def _extract_max_level(file_contents: Union[List[int], List[Tuple[int, int, int]]]):
+    max_level, *pixel_data = file_contents
+    return max_level, pixel_data
+
+
+def _convert_into_tuples(value_list: List[int]) -> List[Tuple[int, int, int]]:
+    return [
+        (value_list[i], value_list[i + 1], value_list[i + 2])
+        for i in range(0, len(value_list), 3)
+    ]
+
+
+def _format_pixel_data(data: Union[List[int], List[Tuple[int, int, int]]], m: int):
+    return [data[i : i + m] for i in range(0, len(data), m)]
+
+
 def parse_file_contents(file_contents: List[str]) -> Dict[str, Any]:
     """Utility function to validate and parse file contents
 
@@ -30,37 +63,27 @@ def parse_file_contents(file_contents: List[str]) -> Dict[str, Any]:
         file_contents (List[str]): File contents as a list of strings
 
     Raises:
-        InvalidHeaderError: [description]
-        InvalidFileError: [description]
         InvalidConfigsError: [description]
         InvalidFileError: [description]
 
     Returns:
         Dict[str, Any]: [description]
     """
-    header, *contents = file_contents  # type: str, List[str]
-    acceptable_headers = ("P1", "P2", "P3")
-    if header not in acceptable_headers:
-        raise InvalidHeaderError(f"Header {header} is not allowed or invalid")
-    try:
-        # All remaining values should be integers
-        m, n, max_grayscale, *data = [
-            int(x) for x in contents
-        ]  # type: int, int, int, List[int]
-    except ValueError:
-        raise InvalidFileError(f"Found invalid values in file contents")
-    if m <= 0 or n <= 0 or max_grayscale <= 0:
+    header, contents = _extract_header(file_contents)  # type: str, List[str]
+    m, n, value_contents = _extract_dimensions(contents)  # type: int, int, List[int]
+    max_level, data = _extract_max_level(value_contents)  # type: int, List[int]
+    if m <= 0 or n <= 0 or max_level <= 0:
         raise InvalidConfigsError(
-            f"Neither m, n or max_grayscale can be negative or zero, found {m=}, {n=}, {max_grayscale=}"
+            f"Neither m, n or max_level can be negative or zero, found {m=}, {n=}, {max_level=}"
         )
     if len(data) != m * n:
         raise InvalidFileError(
             f"Non-matching amount of pixels found, should have {m*n} values, found {len(data)}"
         )
-    pixel_data: List[List[int]] = [data[i : i + m] for i in range(0, len(data), m)]
+    pixel_data: List[List[int]] = _format_pixel_data(data, m)
     return {
         "header": header,
         "dimensions": (m, n),
-        "max_level": max_grayscale,
+        "max_level": max_level,
         "contents": pixel_data,
     }
