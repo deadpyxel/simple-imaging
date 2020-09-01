f2576930928e9d74455055967db68ce82330244b
Robson Cruz <ghost0092@gmail.com>
2020-09-01 19:14:19 -0300

    ci: Update pre-commit hooks

---
+++ tests/test_validations.py
@@ -0,0 +1,20 @@
+import pytest
+
+from simple_imaging.types import validate_operation_level
+
+from .fixtures import invalid_level_types
+
+
+@pytest.mark.parametrize("level", invalid_level_types)
+def test_level_validation_correctly_validates_type(level):
+    assert validate_operation_level(level) == (False, "type")
+
+
+@pytest.mark.parametrize("level", [-1, 256])
+def test_level_validation_correctly_validates_range(level):
+    assert validate_operation_level(level) == (False, "range")
+
+
+@pytest.mark.parametrize("level", [0, 1, 100, 255])
+def test_level_validation_correctly_validates_valid_level(level):
+    assert validate_operation_level(level) == (True, "")
